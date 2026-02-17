5b144b22684ebb8837535be3788170201dd2a387
shaadali001 <alishaad908@gmail.com>
2026-02-17 23:52:44 +0530

    second commit

---
+++ Fundementals_Part1/script.js
@@ -0,0 +1,68 @@
+// alert("Hello World!!!!")
+// console.log('Jonas');
+// console.log(50 + 50 + 908 * 9 - 75);
+
+// let firstName = "ShaadAli";
+// console.log(firstName.length);
+
+
+// ------------DATA TYPES-------------
+
+// console.log(typeof (true));
+// let str = "Hello";
+// console.log(str);
+
+// str = 5;
+// console.log(typeof str);
+
+/*
+let age = prompt("Enter your Age!!");
+if (age < 0) {
+  alert("Enter a valid Age");
+}
+else if (age >= 18) {
+  console.log("You can drive");
+}
+else {
+  console.log("Sorry not Eligable");
+}*/
+
+const Age = 2049;
+const Age2 = 2003;
+
+console.log(Age - Age2);
+
+// let x = 5;
+// x += 10;
+// console.log(x);
+
+// let a = 4;
+// let b = 74;
+// let c = -4;
+// let d = 77;
+// let e = 10;
+
+// console.log(a- b / c + d * e);
+
+// -------------Exercise------------
+// BMI = WEIGHT/HEIGHT M2
+
+// 1
+let MarksWeight = 78;
+let MarksHeight = 1.69;
+
+let JohnWeight = 92;
+let JohanHeight = 1.95;
+
+let MarksBmi = Math.floor((MarksWeight / (MarksHeight) ** 2));
+let JohansBmi = Math.floor((JohnWeight / (JohanHeight) ** 2));
+
+console.log(`MarksBMI = ${MarksBmi} JohansBMI = ${JohansBmi}`);
+
+console.log(MarksBmi > JohansBmi);
+
+// let age = 0;
+// console.log(`I'm Jonas, a ${age} years old teacher!`);
+
+// age > 0 ? console.log(age >= 18 ? "You can Drive" : "You are not Eligable") : console.log("Age not Valid")
+
