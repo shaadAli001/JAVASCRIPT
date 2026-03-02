7db455ec58211e92f6b240e1990768c92d16e05a
shaadali001 <alishaad908@gmail.com>
2026-03-02 23:11:53 +0530

    commit

---
+++ Fundementals_Part2/starter/script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+// let license = false;
+// const test = true;
+
+// ------------------FUNCTIONS-----
+
+// function Students(boy, girl) {
+//   let totalStudents = `No: of Boys ${boy} No:of Girls ${girl} Total = ${boy + girl}`;
+//   return totalStudents;
+// }
+
+// let Class = Students(15, 10);
+// console.log(Class);
+
+
+
+
+
+// Function declaration
+// function sum(num1, num2) {
+//   return num1 + num2;
+// }
+// let result = sum(5, 5);
+// console.log('Sum is = ' + result);
+
+// // Function Expression
+// let sum2 = function (a, b) {
+//   return a + b;
+// }
+
+// let result2 = sum2(10, 10);
+// console.log('Sum is = ' + result2);
+
+
+
+const birthDay = (age) => {
+  return 2037 - age;
+}
+const year = birthDay(2003)
+console.log(year);
+
+
+
+
+
+
+
