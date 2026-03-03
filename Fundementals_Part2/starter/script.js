a0dfa58e9c1595e606fa415e9d5c5eda8bf48ae8
shaadali001 <alishaad908@gmail.com>
2026-03-03 22:31:07 +0530

    pending

--- Fundementals_Part2/starter/script.js
+++ Fundementals_Part2/starter/script.js
@@ -34,11 +34,51 @@
 
 
 
-const birthDay = (age) => {
-  return 2037 - age;
+// const birthDay = (age) => {
+//   return 2037 - age;
+// }
+// const year = birthDay(2003)
+// console.log(year);
+
+// const BirthYear = Number(prompt("Enter Your Age"))
+
+// function calcAge(BirthYear) {
+//   return 2026 - BirthYear;
+// }
+
+// const age = calcAge(BirthYear);
+// console.log(age);
+
+// function fruitCutPiece(nos) {
+//   return nos * Math.floor(Math.random() * 3) + 1;
+// }
+
+// function fruitsProcess(apples, oranges) {
+//   const applePiece = fruitCutPiece(apples);
+//   const orangePiece = fruitCutPiece(oranges);
+//   return `Juice with ${applePiece} apple piece and ${orangePiece} orange piece`;
+// }
+
+// console.log(fruitsProcess(2, 3));
+
+const birthYear = Number(prompt("Enter Your BirthYear"));
+const Name = prompt("Enter Your Name");
+
+function getYear(year) {
+  return 2026 - year;
 }
-const year = birthDay(2003)
-console.log(year);
+
+const data = function yearAfterRetirement(birthYear, name) {
+  const age = getYear(birthYear);
+  const retirement = 65 - age;
+  if (retirement > 0) {
+    return `${retirement} Years left for ${name} Retirement`;
+  }
+  else {
+    return `${name} have Retiered`;
+  }
+}
+console.log(data(birthYear, Name));
 
 
 
